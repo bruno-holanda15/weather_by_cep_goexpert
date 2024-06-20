4c6eed78a762ac7932bfc76cb3fe157ac2b89fd9
Bruno Cavalcante <bruno.cavalcante@fretebras.com.br>
2024-06-20 18:26:23 -0300

    wip add propagation

--- internal/domain/usecase/validateCep.go
+++ internal/domain/usecase/validateCep.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 
 	"github.com/bruno-holanda15/weather_by_cep_goexpert/internal/domain/entity"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
 )
 
 type ValidateCepUsecase struct{}
@@ -22,6 +24,7 @@ func (v *ValidateCepUsecase) Execute(ctx context.Context, input InputWbcUsecase)
 		return OutputWbcUsecase{Err: err}
 	}
 
+	otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(reqWeather.Header))
 	resWeather, err := http.DefaultClient.Do(reqWeather)
 	if err != nil {
 		return OutputWbcUsecase{Err: err}
